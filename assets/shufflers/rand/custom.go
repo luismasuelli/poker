a05f98077649b36e0abcc3ec0bbb8244df626431
luismasuelli <luis@triplemint.com>
2020-06-05 16:03:40 -0500

    Adding the shufflers using math/rand.

--- assets/shufflers/rand/custom.go
+++ assets/shufflers/rand/custom.go
@@ -23,38 +23,18 @@ func (shuffler *CustomShuffler) Shuffle(deck cards.Deck) {
 	shuffler.randObj.Shuffle(deck.Len(), deck.Swap)
 }
 
-// A custom shuffler option for the NewShuffler
-// constructor.
-type ShufflerOption func(shuffler *CustomShuffler)
-
-// Option to create a shuffler that always seeds
-// on each shuffle, using the current time.
-func TimeSeedOnShuffle(shuffler *CustomShuffler) {
-	shuffler.timeSeed = true
-}
-
-// Option to set the initial seed of the shuffler.
-func InitSeedWith(seed int64) ShufflerOption {
-	return func(shuffler *CustomShuffler) {
-		shuffler.randObj.Seed(seed)
-	}
-}
-
 // Creates a new rand-based shuffler, with option to
 // choose a rand-standard source (using nil as source)
-// or a custom one. Additional options may be given
+// or a custom one. Additional options will be given
 // to set the initial source's seed and/or tell the
 // shuffler to continuously seed on current time.
-func NewShuffler(source rand.Source, options ...ShufflerOption) *CustomShuffler {
+func NewShuffler(source rand.Source, timeSeed bool, initialSeed int64) *CustomShuffler {
 	if source == nil {
-		source = rand.NewSource(1)
+		source = rand.NewSource(initialSeed)
 	}
 	shuffler := &CustomShuffler{
 		randObj: rand.New(source),
-		timeSeed: false,
-	}
-	for _, option := range options {
-		option(shuffler)
+		timeSeed: timeSeed,
 	}
 	return shuffler
 }
---
+++ assets/shufflers/rand/default.go
@@ -0,0 +1,31 @@
+package rand
+
+import (
+	"github.com/luismasuelli/poker/assets/cards"
+	"time"
+	"math/rand"
+)
+
+// A default shuffler uses the default, global, rand
+// functions for Seed and Shuffle. This works similar
+// to the custom shuffler, except that it works with
+// the global source, which is safe in concurrency.
+type DefaultShuffler struct {
+	timeSeed bool
+}
+
+// Shuffles a deck using its Len and Swap methods
+// in the underlying rand object.
+func (shuffler *DefaultShuffler) Shuffle(deck cards.Deck) {
+	if shuffler.timeSeed {
+		rand.Seed(time.Now().UTC().UnixNano())
+	}
+	rand.Shuffle(deck.Len(), deck.Swap)
+}
+
+// Creates a new rand-based Shuffler, which uses the
+// standard Seed and Shuffle functions, which may seed
+// with the current time on each shuffle, or not.
+func NewDefaultShuffler(timeSeed bool) *DefaultShuffler {
+	return &DefaultShuffler{timeSeed}
+}
