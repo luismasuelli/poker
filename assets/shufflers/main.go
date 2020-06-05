587fb7ce87cc2aa271f53af6a9f9c91a46a612e1
luismasuelli <luis@triplemint.com>
2020-06-05 14:45:47 -0500

    Adding the shuffler interface.

---
+++ assets/shufflers/main.go
@@ -0,0 +1,12 @@
+package shufflers
+
+import "github.com/luismasuelli/poker/assets/cards"
+
+// Shufflers provide only one method to shuffle any
+// given deck (regardless the deck type). The
+// shuffling is always in-place, and it will make
+// use of the .Swap(i, j) and .Len() functions
+// inside a deck.
+type Shuffler interface {
+	Shuffle(deck cards.Deck)
+}
