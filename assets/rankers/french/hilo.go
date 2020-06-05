1a179f16042d798fdea0f77dc788bc85ca899017
luismasuelli <luis@triplemint.com>
2020-06-05 16:23:53 -0500

    Adding hand rankers.

---
+++ assets/rankers/french/hilo.go
@@ -0,0 +1,41 @@
+package french
+
+import (
+	"github.com/luismasuelli/poker/assets/cards"
+	"github.com/luismasuelli/poker/assets/cards/french"
+	"github.com/luismasuelli/poker/assets/rankers"
+)
+
+// Ranks a french card, all of them but wildcards
+// (they are forbidden in this ranking), with A-high.
+func HighRanker(card cards.Card) uint8 {
+	frenchCard := card.(french.Card)
+	if frenchCard.Suit() == french.Wildcard {
+		panic(rankers.ErrInvalidCard)
+	}
+	return frenchCard.Rank() - 2
+}
+
+// Ranks a french card, all of them but wildcards
+// (they are forbidden in this ranking), with A-low.
+func LowRanker(card cards.Card) uint8 {
+	frenchCard := card.(french.Card)
+	if frenchCard.Suit() == french.Wildcard {
+		panic(rankers.ErrInvalidCard)
+	} else if frenchCard.Rank() == 14 {
+		return 0
+	} else {
+		return frenchCard.Rank() - 1
+	}
+}
+
+// Ranks a french card with A-high. Wildcards count
+// as A, and are ranked accordingly.
+func Caribbean(card cards.Card) uint8 {
+	frenchCard := card.(french.Card)
+	if frenchCard.Suit() == french.Wildcard || frenchCard.Rank() == 14 {
+		return 0
+	} else {
+		return frenchCard.Rank() - 1
+	}
+}
