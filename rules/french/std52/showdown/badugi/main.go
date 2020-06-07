e278d54249d27674b27306f0ed2b6bfbc2237477
Luis Masuelli <luismasuelli@hotmail.com>
2020-06-07 15:31:51 -0500

    Badugi hand comparison.

--- rules/french/std52/showdown/badugi/main.go
+++ rules/french/std52/showdown/badugi/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/luismasuelli/poker/assets/cards/french"
 )
 
+// Masks of 1-hot rank and 1-hot suit.
 var cardMasks = (func() [52]uint32 {
 	result := [52]uint32{}
 	index := 0
@@ -24,7 +25,10 @@ var cardMasks = (func() [52]uint32 {
 	return result
 })()
 
-const fullCardMask = 0x1111111111111111100
+// Full mask telling the space the 1-hot bits are relevant.
+const fullMask = 0x1111111111111111100
+const rankMask = 0x1111111111111000000
+const indxMask = 0x0000000000000000011
 
 // A Badugi evaluator tries to return the best hand
 // with the LEAST possible power. "A234" completely
@@ -32,11 +36,14 @@ const fullCardMask = 0x1111111111111111100
 // hand. As an expected precondition, which will not
 // be tested, the hand must have 4 french cards in a
 // "standard 52" set.
-func Power(hand []cards.Card, community []cards.Card) (best []cards.Card, power uint64) {
-	var handMask = [4]uint32{}
+//
+// The return value is: (best, power) where
+func Power(hand []cards.Card, community []cards.Card) (best uint32, power uint64) {
+	handMask := [4]uint32{}
+	removedCards := 0
 	// encoding each card as (1, mask, index).
-	for index, card := range hand {
-		handMask[index] = 1<<19 | cardMasks[card.(french.Card)] | uint32(index&3)
+	for index, card := range hand[0:4] {
+		handMask[index] = cardMasks[card.(french.Card)] | uint32(index)
 	}
 	// a manual sort over these uint32 values.
 	for i := 0; i < 3; i++ {
@@ -51,12 +58,13 @@ func Power(hand []cards.Card, community []cards.Card) (best []cards.Card, power 
 	for i := 0; i < 3; i++ {
 		count := 0
 		for j := i + 1; j < 4; j++ {
-			if handMask[i]&handMask[j]&fullCardMask != 0 {
+			if handMask[i]&handMask[j]&fullMask != 0 {
 				count++
 			}
 		}
 		if count > 1 {
 			handMask[i] = 0
+			removedCards++
 		}
 	}
 	// Then check cards that match ONE other card, and also
@@ -64,14 +72,29 @@ func Power(hand []cards.Card, community []cards.Card) (best []cards.Card, power 
 	for i := 0; i < 3; i++ {
 		count := 0
 		for j := i + 1; j < 4; j++ {
-			if handMask[i]&handMask[j]&fullCardMask != 0 {
+			if handMask[i]&handMask[j]&fullMask != 0 {
 				count++
 			}
 		}
 		if count > 0 {
 			handMask[i] = 0
+			removedCards++
 		}
 	}
-	// Now, compute the power of the hand.
-	// TODO.
+	// Now, compute the power of the hand. The power will be
+	// comparable to other hands (the greater, the worst) but
+	// will not serve to tell the distribution of win/lose
+	// hands with respect to it.
+	// Also, the indices will be computed as well. The indices
+	// are an uint32 flagset telling which indices (marked as
+	// set bits) make the best hand.
+	best = 0
+	power = uint64(removedCards << 19)
+	for _, card := range handMask {
+		power |= uint64(card & rankMask)
+		if card != 0 {
+			best |= 1 << (card & indxMask)
+		}
+	}
+	return
 }
