8ecffa3001cd82945ef34203b67b35a21a9fee90
Luis Masuelli <luismasuelli@hotmail.com>
2020-06-07 22:00:41 -0500

    Badugi tests - full

--- rules/french/std52/showdown/badugi/main_test.go
+++ rules/french/std52/showdown/badugi/main_test.go
@@ -21,7 +21,7 @@ func TestNaiveCases(t *testing.T) {
 	testBadugi(t, 0b0111, 0b010000000000111000000, CA, H2, D3, S3)
 	testBadugi(t, 0b0101, 0b101100000000000000000, CK, HK, HQ, SQ)
 	testBadugi(t, 0b1010, 0b100100000000001000000, CK, CQ, CA, SA)
-	// testBadugi(t, CK, CQ, CA, SQ)
-	// testBadugi(t, CK, CQ, CA, SK)
-	// testBadugi(t, CK, CQ, SK, SQ)
+	testBadugi(t, 0b1100, 0b100100000000001000000, CK, CQ, CA, SQ)
+	testBadugi(t, 0b1100, 0b101000000000001000000, CK, CQ, CA, SK)
+	testBadugi(t, 0b1001, 0b101100000000000000000, CK, CQ, SK, SQ)
 }
