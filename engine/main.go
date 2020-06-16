88e67a0236b58e2b2eb211de064d377be843166e
luismasuelli <luis@triplemint.com>
2020-06-16 15:28:30 -0500

    Adding preliminary types.

---
+++ engine/main.go
@@ -0,0 +1,18 @@
+package engine
+
+// There are many different games that start and end, and will never
+// be created again (but perhaps different instances of them) like
+// tournaments, and there are other games that never end, but can
+// be shutdown and recreated (with the same identity) like cash tables.
+// This said, there is a notion of key or identity for games.
+type GameID interface{}
+
+// All the games have at least one table. A typical game will have
+// few tables, at least 1. Multi-table tournaments can have thousands
+// of tables instead. TableID 0xffffffff is reserved for lobby in MTT.
+// TableID 0 means "no table in particular".
+type TableID uint32
+
+// Tables will have a small number of seats. SeatID 0 means "no seat
+// in particular".
+type SeatID uint8
