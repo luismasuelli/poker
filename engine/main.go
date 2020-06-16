9435ece9ef74876a3c5f3341e93eff1f33523c17
luismasuelli <luis@triplemint.com>
2020-06-16 15:34:17 -0500

    Adding preliminary types.

--- engine/main.go
+++ engine/main.go
@@ -16,3 +16,8 @@ type TableID uint32
 // Tables will have a small number of seats. SeatID 0 means "no seat
 // in particular".
 type SeatID uint8
+
+// Player requests will be big numbers to enumerate transactions that
+// can be tracked in future messages. RequestID 0 means "no request
+// in particular" and should never be used, to avoid confusion.
+type RequestID uint64
