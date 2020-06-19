d35d61d9dbebe75e25d5d201df7b7d08a7392691
luismasuelli <luis@triplemint.com>
2020-06-19 18:04:37 -0500

    Creating an interface and renaming the Seat class to BaseSeat.

--- engine/games/tables/seats/main.go
+++ engine/games/tables/seats/main.go
@@ -33,11 +33,47 @@ const (
 	SitOut Flags = 1
 )
 
+type Seat interface {
+	// Data to be retrieved.
+	// The player.
+	Player() players.Player
+	// The stack.
+	Stack()  uint64
+	// The status.
+	Status() Status
+	// The flags.
+	Flags()  Flags
+
+	// Sits a player, if empty.
+	Sit(player players.Player, stack uint64) bool
+	// Pops a player, if occupied.
+	Pop() (player players.Player, stack uint64)
+	// Sets the status, if occupied.
+	// It is not allowed to set the Free state.
+	SetStatus(status Status) bool
+	// Sets a flag.
+	SetFlag(flag Flags) bool
+	// Clears a flag.
+	ClearFlag(flag Flags) bool
+	// Takes chips, if occupied and affordable.
+	TakeChips(chips uint64) bool
+	// Gives chips, if occupied and not overflowing.
+	GiveChips(chips uint64) bool
+	// Sets stack, if occupied.
+	SetStack(chips uint64) bool
+}
+
 // Seats are managed by the tables. They
 // keep a reference to the player and to
 // each aspect of their participation in
-// the table.
-type Seat struct {
+// the table. This class could and should
+// be enabled to support more flags that
+// may be related to subsets of the rules,
+// e.g. whether there are blinds being
+// used, and even the underlying data
+// related to the algorithm for blinds
+// management (simple, moving, or dead).
+type BaseSeat struct {
 	player players.Player
 	status Status
 	flags  Flags
@@ -45,12 +81,12 @@ type Seat struct {
 }
 
 // Gets the underlying sit player.
-func (seat *Seat) Player() players.Player {
+func (seat *BaseSeat) Player() players.Player {
 	return seat.player
 }
 
 // Gets the stack of this seat.
-func (seat *Seat) Stack() uint64 {
+func (seat *BaseSeat) Stack() uint64 {
 	return seat.stack
 }
 
@@ -58,13 +94,13 @@ func (seat *Seat) Stack() uint64 {
 // only 5 statuses in poker games, although
 // flags may condition when and how these
 // states are changed.
-func (seat *Seat) Status() Status {
+func (seat *BaseSeat) Status() Status {
 	return seat.status
 }
 
 // Returns the flags of this seat. So far,
 // this only involves the "sit out" button.
-func (seat *Seat) Flags() Flags {
+func (seat *BaseSeat) Flags() Flags {
 	return seat.flags
 }
 
@@ -73,7 +109,7 @@ func (seat *Seat) Flags() Flags {
 // is already sit. It is recommended for
 // this method to be redefined on composition
 // if more stuff is to be changed on sit.
-func (seat *Seat) Sit(player players.Player, stack uint64) bool {
+func (seat *BaseSeat) Sit(player players.Player, stack uint64) bool {
 	if seat.status == Free || player == nil || stack == 0 {
 		return false
 	} else {
@@ -89,7 +125,7 @@ func (seat *Seat) Sit(player players.Player, stack uint64) bool {
 // It is recommended for this method to be
 // redefined on composition if more stuff is
 // to be changed on pop.
-func (seat *Seat) Pop() (player players.Player, stack uint64) {
+func (seat *BaseSeat) Pop() (player players.Player, stack uint64) {
 	player = seat.player
 	stack = seat.stack
 	seat.player = nil
@@ -102,7 +138,7 @@ func (seat *Seat) Pop() (player players.Player, stack uint64) {
 // Sets the status of this seat. The "free"
 // status cannot set, and cannot replaced,
 // by this method.
-func (seat *Seat) SetStatus(status Status) bool {
+func (seat *BaseSeat) SetStatus(status Status) bool {
 	if seat.player == nil || status == Free {
 		// Cannot set the status of a table
 		// without player, and cannot set
@@ -117,7 +153,7 @@ func (seat *Seat) SetStatus(status Status) bool {
 
 // Sets a flag on this seat. This can only
 // be done to non-free seats.
-func (seat *Seat) SetFlag(flag Flags) bool {
+func (seat *BaseSeat) SetFlag(flag Flags) bool {
 	if seat.player == nil {
 		return false
 	} else {
@@ -128,7 +164,7 @@ func (seat *Seat) SetFlag(flag Flags) bool {
 
 // Clears a flag on this seat. This can only
 // be done to non-free seats.
-func (seat *Seat) ClearFlag(flag Flags) bool {
+func (seat *BaseSeat) ClearFlag(flag Flags) bool {
 	if seat.player == nil {
 		return false
 	} else {
@@ -140,7 +176,7 @@ func (seat *Seat) ClearFlag(flag Flags) bool {
 // Takes chips from the seat. This can only
 // be done to non-free seats that can afford
 // the specified chips amount.
-func (seat *Seat) TakeChips(chips uint64) bool {
+func (seat *BaseSeat) TakeChips(chips uint64) bool {
 	if seat.player == nil || seat.stack < chips {
 		return false
 	} else {
@@ -155,7 +191,7 @@ func (seat *Seat) TakeChips(chips uint64) bool {
 // recommended that table levels and user
 // accounts are defensively restricted to
 // reach a case like this.
-func (seat *Seat) GiveChips(chips uint64) bool {
+func (seat *BaseSeat) GiveChips(chips uint64) bool {
 	if seat.player == nil || seat.stack > (^(uint64(0) - chips)) {
 		return false
 	} else {
@@ -170,7 +206,7 @@ func (seat *Seat) GiveChips(chips uint64) bool {
 // may cause overflow as well when receiving
 // more money or popping the seat in a cash
 // 1-table game.
-func (seat *Seat) SetStack(chips uint64) bool {
+func (seat *BaseSeat) SetStack(chips uint64) bool {
 	if seat.player == nil {
 		return false
 	} else {
