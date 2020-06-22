567b947f6f3114fa77ae16826dea3796f85878ce
luismasuelli <luis@triplemint.com>
2020-06-22 09:49:14 -0500

    Refactoring the broadcasters.

---
+++ engine/games/tables/environment/main.go
@@ -0,0 +1,64 @@
+package environment
+
+import (
+	"github.com/luismasuelli/poker-go/engine/games/tables/seats"
+	"github.com/luismasuelli/poker-go/engine/misc"
+)
+
+// Combines the seats and watchers
+// broadcasters in just one.
+type Broadcaster struct {
+	seats []seats.Seat
+	watchers map[misc.Notifiable]bool
+}
+
+// Creates a new environment broadcaster with
+// the involved seats and an empty list of
+// watchers.
+func NewBroadcaster(seats []seats.Seat) *Broadcaster {
+	return &Broadcaster{
+		seats,
+		map[misc.Notifiable]bool{},
+	}
+}
+
+// Notifies to both sit players and watchers.
+func (broadcaster *Broadcaster) Notify(message interface{}) {
+	for _, seat := range broadcaster.seats {
+		if player := seat.Player(); player != nil {
+			func() {
+				defer func() { recover() }()
+				player.Notify(message)
+			}()
+		}
+	}
+	for watcher, _ := range broadcaster.watchers {
+		func() {
+			defer func() { recover() }()
+			watcher.Notify(message)
+		}()
+	}
+}
+
+// Registers a new watcher. Returns false
+// if already registered, or nil.
+func (broadcaster *Broadcaster) Register(watcher misc.Notifiable) bool {
+	if watcher == nil {
+		return false
+	} else if _, ok := broadcaster.watchers[watcher]; ok {
+		return false
+	} else {
+		broadcaster.watchers[watcher] = true
+		return true
+	}
+}
+
+// Unregisters a watcher. Returns false if absent.
+func (broadcaster *Broadcaster) Unregister(watcher misc.Notifiable) bool {
+	if _, ok := broadcaster.watchers[watcher]; !ok {
+		return false
+	} else {
+		delete(broadcaster.watchers, watcher)
+		return true
+	}
+}
