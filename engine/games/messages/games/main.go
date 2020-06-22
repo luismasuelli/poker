efa935110f0e16a9a96b4845d4ae2ff36c98e18c
luismasuelli <luis@triplemint.com>
2020-06-22 14:15:51 -0500

    Refactoring the game message.

--- engine/games/messages/games/main.go
+++ engine/games/messages/games/main.go
@@ -1,11 +1,13 @@
 package games
 
-import "github.com/luismasuelli/poker-go/engine/games/messages"
-
 // Messages related to a game will hold their
 // ID. This is only meaningful when a user is
 // actually observing a game  status.
 type GameMessage struct {
-	messages.Message
-	GameID interface{}
+	// The ID of the game this content is
+	// related to.
+	GameID  interface{}
+
+	// Content of the message.
+	Content interface{}
 }
