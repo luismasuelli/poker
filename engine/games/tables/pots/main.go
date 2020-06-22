fb47e99cc601baf64aae0f3f87cce250f360584c
luismasuelli <luis@triplemint.com>
2020-06-22 17:54:19 -0500

    Adding methods to split the quantities.

--- engine/games/tables/pots/main.go
+++ engine/games/tables/pots/main.go
@@ -36,13 +36,49 @@ func (pot *Pot) SeatHasLeft(seat seats.Seat) {
 	delete(pot.seats, seat)
 }
 
+// Gets the pot amount.
+func (pot *Pot) Amount() uint64 {
+	return pot.amount
+}
+
+// Splits the pot in sub-pots of the given
+// quantities and the same participants. If
+// the quantities surpass the whole pot, a
+// final pot is added with the remaining
+// quantities.
+func (pot *Pot) Split(amounts ...uint64) []*Pot {
+	remainingAmount := pot.amount
+	splitPots := make([]*Pot, 0)
+
+	// Iterating involves consuming all the amounts
+	// that can be consumed (i.e. are lower than the
+	// total amount in the pot). The moment we find
+	// a pot equal or larger to the remaining pot,
+	// or the moment we consume all the pots, we
+	// exit the loop.
+	for _, amount := range amounts {
+		if amount < remainingAmount {
+			splitPots = append(splitPots, &Pot{amount, pot.seats})
+			remainingAmount -= amount
+		} else {
+			break
+		}
+	}
+	// In the end, we still have some remaining
+	// amount. This will make us have such amount
+	// be added to the end of the list, as a new
+	// and final pot.
+	splitPots = append(splitPots, &Pot{remainingAmount, pot.seats})
+	return splitPots
+}
+
 // Divides equally the amount of the pot among
 // the winners that are involved with this pot.
 // An intersection of such players is considered,
 // and the money is divided among them. If no
 // given winners are involved in this pot, then
 // the result is (0, 0).
-func (pot *Pot) Split(winners []seats.Seat) ([]seats.Seat, uint64, uint8) {
+func (pot *Pot) Award(winners []seats.Seat) ([]seats.Seat, uint64, uint8) {
 	// Compute intersections to keep only the
 	// players that are both winners and also
 	// involved in the pot.
