70b3825cf0e90a3e24010cca619abf8646bae727
Luis Masuelli <luismasuelli@hotmail.com>
2020-06-21 20:36:30 -0500

    Changing the contract of seats.

--- engine/games/tables/seats/main.go
+++ engine/games/tables/seats/main.go
@@ -1,6 +1,7 @@
 package seats
 
 import (
+	"errors"
 	"github.com/luismasuelli/poker-go/engine/games/cards"
 	"github.com/luismasuelli/poker-go/engine/players"
 )
@@ -52,32 +53,32 @@ type Seat interface {
 	Flags() Flags
 
 	// Sits a player, if empty.
-	Sit(player players.Player, stack uint64) bool
+	Sit(player players.Player, stack uint64) error
 	// Pops a player, if occupied.
 	Pop() (player players.Player, stack uint64)
 	// Sets the status, if occupied.
 	// It is not allowed to set the Free state.
-	SetStatus(status Status) bool
+	SetStatus(status Status) error
 	// Sets a flag.
-	SetFlag(flag Flags) bool
+	SetFlag(flag Flags) error
 	// Clears a flag.
-	ClearFlag(flag Flags) bool
+	ClearFlag(flag Flags) error
 	// Takes chips, if occupied and affordable.
-	SubStack(chips uint64) bool
+	SubStack(chips uint64) error
 	// Gives chips, if occupied and not overflowing.
-	AddStack(chips uint64) bool
+	AddStack(chips uint64) error
 	// Sets stack, if occupied.
-	SetStack(chips uint64) bool
+	SetStack(chips uint64) error
 	// Takes chips from the pot, if occupied and affordable.
-	SubPot(chips uint64) bool
+	SubPot(chips uint64) error
 	// Gives chips to the pot, if occupied and not overflowing.
-	AddPot(chips uint64) bool
+	AddPot(chips uint64) error
 	// Sets pot, if occupied.
-	SetPot(chips uint64) bool
+	SetPot(chips uint64) error
 	// Adds cards to the hand.
-	AddCards(seatCards []*SeatCard) bool
+	AddCards(seatCards []*SeatCard) error
 	// Takes cards (by indices) from the hand.
-	RemoveCards(indices []int)
+	RemoveCards(indices []int) error
 	// Gets the actual cards.
 	Cards(revealed bool) []cards.Card
 }
@@ -90,16 +91,6 @@ type Seat interface {
 type SeatCard struct {
 	card  cards.Card
 	shown bool
-}
-
-// Marks the card as shown.
-func (seatCard *SeatCard) Show() {
-	seatCard.shown = true
-}
-
-// Marks the card as hidden.
-func (seatCard *SeatCard) Hide() {
-	seatCard.shown = false
 }
 
 // Tells whether the card is shown or
@@ -179,19 +170,27 @@ func (seat *BaseSeat) Flags() Flags {
 	return seat.flags
 }
 
+var ErrCannotSitNilPlayer = errors.New("cannot sit a nil player")
+var ErrCannotSitPlayerOnOccupiedSeat = errors.New("cannot sit a player on an occupied seat")
+var ErrCannotSitPlayerWithEmptyStack = errors.New("cannot sit a player with an empty stack")
+
 // Tries to sit a player, if given and with
 // a non-empty stack, and only if no player
 // is already sit. It is recommended for
 // this method to be redefined on composition
 // if more stuff is to be changed on sit.
-func (seat *BaseSeat) Sit(player players.Player, stack uint64) bool {
-	if seat.status == Free || player == nil || stack == 0 {
-		return false
+func (seat *BaseSeat) Sit(player players.Player, stack uint64) error {
+	if seat.status != Free {
+		return ErrCannotSitPlayerOnOccupiedSeat
+	} else if player == nil {
+		return ErrCannotSitNilPlayer
+	} else if stack == 0 {
+		return ErrCannotSitPlayerWithEmptyStack
 	} else {
 		seat.player = player
 		seat.stack = stack
 		seat.status = Waiting
-		return true
+		return nil
 	}
 }
 
@@ -205,59 +204,68 @@ func (seat *BaseSeat) Pop() (player players.Player, stack uint64) {
 	stack = seat.stack
 	seat.player = nil
 	seat.stack = 0
+	seat.pot = 0
 	seat.status = Free
 	seat.flags = Nothing
 	seat.cards = make([]*SeatCard, 0)
 	return
 }
 
+var ErrCannotSetFreeStatus = errors.New("cannot manually set the 'free' status in a seat")
+var ErrCannotChangeStatusOnEmptySeat = errors.New("cannot manually change status on empty seats")
+
 // Sets the status of this seat. The "free"
 // status cannot set, and cannot replaced,
 // by this method.
-func (seat *BaseSeat) SetStatus(status Status) bool {
-	if seat.player == nil || status == Free {
-		// Cannot set the status of a table
-		// without player, and cannot set
-		// the status of a table with player
-		// to Free.
-		return false
-	} else {
-		seat.status = status
-		return true
+func (seat *BaseSeat) SetStatus(status Status) error {
+	if seat.player == nil {
+		return ErrCannotChangeStatusOnEmptySeat
+	} else if status == Free {
+		return ErrCannotSetFreeStatus
 	}
+	seat.status = status
+	return nil
 }
+
+var ErrCannotChangeFlagsOnEmptySeat = errors.New("cannot manually change flags on empty seats")
 
 // Sets a flag on this seat. This can only
 // be done to non-free seats.
-func (seat *BaseSeat) SetFlag(flag Flags) bool {
+func (seat *BaseSeat) SetFlag(flag Flags) error {
 	if seat.player == nil {
-		return false
+		return ErrCannotChangeFlagsOnEmptySeat
 	} else {
 		seat.flags |= flag
-		return true
+		return nil
 	}
 }
 
 // Clears a flag on this seat. This can only
 // be done to non-free seats.
-func (seat *BaseSeat) ClearFlag(flag Flags) bool {
+func (seat *BaseSeat) ClearFlag(flag Flags) error {
 	if seat.player == nil {
-		return false
+		return ErrCannotChangeFlagsOnEmptySeat
 	} else {
 		seat.flags &= ^flag
-		return true
+		return nil
 	}
 }
+
+var ErrCannotChangeStackOnEmptySeat = errors.New("cannot manually change stack on empty seats")
+var ErrCannotAffordChipsSubtraction = errors.New("cannot afford the specified chips subtraction")
+var ErrCannotContainChipsAddition = errors.New("cannot contain the specified chips addition")
 
 // Takes chips from the seat. This can only
 // be done to non-free seats that can afford
 // the specified chips amount.
-func (seat *BaseSeat) SubStack(chips uint64) bool {
-	if seat.player == nil || seat.stack < chips {
-		return false
+func (seat *BaseSeat) SubStack(chips uint64) error {
+	if seat.player == nil {
+		return ErrCannotChangeStackOnEmptySeat
+	} else if seat.stack < chips {
+		return ErrCannotAffordChipsSubtraction
 	} else {
 		seat.stack -= chips
-		return true
+		return nil
 	}
 }
 
@@ -267,12 +275,14 @@ func (seat *BaseSeat) SubStack(chips uint64) bool {
 // recommended that table levels and user
 // accounts are defensively restricted to
 // reach a case like this.
-func (seat *BaseSeat) AddStack(chips uint64) bool {
-	if seat.player == nil || seat.stack > (^(uint64(0) - chips)) {
-		return false
+func (seat *BaseSeat) AddStack(chips uint64) error {
+	if seat.player == nil {
+		return ErrCannotChangeStackOnEmptySeat
+	} else if seat.stack > (^(uint64(0) - chips)) {
+		return ErrCannotContainChipsAddition
 	} else {
 		seat.stack += chips
-		return true
+		return nil
 	}
 }
 
@@ -282,25 +292,31 @@ func (seat *BaseSeat) AddStack(chips uint64) bool {
 // may cause overflow as well when receiving
 // more money or popping the seat in a cash
 // 1-table game.
-func (seat *BaseSeat) SetStack(chips uint64) bool {
+func (seat *BaseSeat) SetStack(chips uint64) error {
 	if seat.player == nil {
-		return false
+		return ErrCannotChangeStackOnEmptySeat
 	} else {
 		seat.stack = chips
-		return true
+		return nil
 	}
 }
+
+var ErrCannotChangePotOnEmptySeat = errors.New("cannot manually change pot on empty seats")
+var ErrCannotAffordPotChipsSubtraction = errors.New("cannot afford the specified pot chips subtraction")
+var ErrCannotContainPotChipsAddition = errors.New("cannot contain the specified pot chips addition")
 
 // Takes chips from the seat's pot. This can
 // only be done to non-free seats that can
 // afford the specified chips amount from their
 // pots.
-func (seat *BaseSeat) SubPot(chips uint64) bool {
-	if seat.player == nil || seat.pot < chips {
-		return false
+func (seat *BaseSeat) SubPot(chips uint64) error {
+	if seat.player == nil {
+		return ErrCannotChangePotOnEmptySeat
+	} else if seat.pot < chips {
+		return ErrCannotAffordPotChipsSubtraction
 	} else {
 		seat.pot -= chips
-		return true
+		return nil
 	}
 }
 
@@ -310,12 +326,14 @@ func (seat *BaseSeat) SubPot(chips uint64) bool {
 // It is recommended that table levels and
 // user accounts are defensively restricted
 // to reach a case like this.
-func (seat *BaseSeat) AddPot(chips uint64) bool {
-	if seat.player == nil || seat.pot > (^(uint64(0) - chips)) {
-		return false
+func (seat *BaseSeat) AddPot(chips uint64) error {
+	if seat.player == nil {
+		return ErrCannotChangePotOnEmptySeat
+	} else if seat.pot > (^(uint64(0) - chips)) {
+		return ErrCannotContainPotChipsAddition
 	} else {
 		seat.pot += chips
-		return true
+		return nil
 	}
 }
 
@@ -325,57 +343,65 @@ func (seat *BaseSeat) AddPot(chips uint64) bool {
 // may cause overflow as well when receiving
 // more money or popping the seat in a cash
 // 1-table game.
-func (seat *BaseSeat) SetPot(chips uint64) bool {
+func (seat *BaseSeat) SetPot(chips uint64) error {
 	if seat.player == nil {
-		return false
+		return ErrCannotChangePotOnEmptySeat
 	} else {
 		seat.pot = chips
-		return true
+		return nil
 	}
 }
 
+var ErrCannotAddCardsToEmptySeat = errors.New("cannot add cards to empty seats")
+var ErrCannotAddNilOrEmptyCardToSeat = errors.New("cannot add a nil or empty card to a seat")
+var ErrCannotRemoveCardsFromEmptySeat = errors.New("cannot remove cards from empty seats")
+
 // Adds non-nil cards.
-func (seat *BaseSeat) AddCards(seatCards []*SeatCard) bool {
-	if len(seatCards) == 0 {
-		return true
-	}
-
-	for _, seatCard := range seatCards {
-		if seatCard == nil || seatCard.Card() == nil {
-			return false
+func (seat *BaseSeat) AddCards(seatCards []*SeatCard) error {
+	if seat.player == nil {
+		return ErrCannotAddCardsToEmptySeat
+	} else if len(seatCards) == 0 {
+		return nil
+	} else {
+		for _, seatCard := range seatCards {
+			if seatCard == nil || seatCard.Card(true) == nil {
+				return ErrCannotAddNilOrEmptyCardToSeat
+			}
 		}
-	}
 
-	seat.cards = append(seat.cards, seatCards...)
-	return true
+		seat.cards = append(seat.cards, seatCards...)
+		return nil
+	}
 }
 
 // Removes cards by indices. A []int{-1} array
 // clears the hand.
-func (seat *BaseSeat) RemoveCards(indices []int) {
-	// Test []int{-1} special case.
-	if len(indices) == 1 && indices[0] < 0 {
+func (seat *BaseSeat) RemoveCards(indices []int) error {
+	if seat.player == nil {
+		return ErrCannotAddCardsToEmptySeat
+	} else if len(indices) == 1 && indices[0] < 0 {
+		// Test []int{-1} special case.
 		seat.cards = make([]*SeatCard, 0)
-		return
-	}
-
-	cardsLen := len(seat.cards)
-	indicesSet := map[int]bool{}
-	for _, index := range indices {
-		if index < cardsLen && index >= 0 {
-			indicesSet[index] = true
+	} else {
+		cardsLen := len(seat.cards)
+		indicesSet := map[int]bool{}
+		for _, index := range indices {
+			if index < cardsLen && index >= 0 {
+				indicesSet[index] = true
+			}
 		}
-	}
 
-	newCards := make([]*SeatCard, cardsLen-len(indicesSet))
-	newIndex := 0
-	for index, card := range seat.cards {
-		if _, ok := indicesSet[index]; !ok {
-			newCards[newIndex] = card
-			newIndex++
+		newCards := make([]*SeatCard, cardsLen-len(indicesSet))
+		newIndex := 0
+		for index, card := range seat.cards {
+			if _, ok := indicesSet[index]; !ok {
+				newCards[newIndex] = card
+				newIndex++
+			}
 		}
+		seat.cards = newCards
 	}
-	seat.cards = newCards
+	return nil
 }
 
 // Gets the actual cards from the seat.
