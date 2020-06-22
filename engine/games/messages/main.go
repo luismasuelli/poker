7b5a31c15a5f1cba5fc730b200502e8bd20a53e7
luismasuelli <luis@triplemint.com>
2020-06-22 14:14:04 -0500

    Refactoring the message type.

--- engine/games/messages/main.go
+++ engine/games/messages/main.go
@@ -1,13 +1,21 @@
 package messages
 
 type Message struct {
+	// Message content.
+	Body       interface{}
+
+	// Some messages can be in response to other
+	// messages sent by client. A value of 0
+	// means this message is not replying.
+	ReplyingTo uint64
+
 	// Some messages can have an underlying reason
 	// to do what they do (e.g. administrative
 	// actions).
-	Reason  string
-	Details interface{}
-	// Some messages can be in response to other
-	// messages sent by client. A value of 0
-	// means this message is not replying-
-	ReplyingTo uint64
+
+	// Reason code for the message, if used.
+	Reason     string
+
+	// Reason details for the message, if used.
+	Details    interface{}
 }
